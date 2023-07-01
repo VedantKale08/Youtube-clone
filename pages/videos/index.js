516343cdd451b7345963f1e05398080742114dee
vedant.medisage <vedant.medisage@gmail.com>
2023-07-01 13:46:18 +0530

    eslint issue

--- pages/videos/index.js
+++ pages/videos/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import Videocard from '@/Components/Videocard';
 import { isEmpty } from 'lodash';
 import React, { useEffect, useState } from 'react'
